d8f10661ec5e7afed9029bf5a349a96ee7fb9534
Mariama DIALLO <diallo.mbobo108@gmail.com>
2023-04-24 13:36:30 +0200

    Initialisation gitignore

---
+++ test/ilu2/WelcomeTest.java
@@ -0,0 +1,16 @@
+package ilu2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class WelcomeTest {
+
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
